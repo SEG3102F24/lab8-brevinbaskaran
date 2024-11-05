d906d407b62728631295515d304c52c13bb94661
brevinbaskaran <brevin.baskaran@outlook.com>
2024-11-05 15:08:43 -0500

    first commit

--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -3,6 +3,22 @@ package seg3x02.employeeGql.resolvers
 import org.springframework.stereotype.Controller
 
 @Controller
-class EmployeesResolver {
+class EmployeesResolver(private val employeeRepository: EmployeeRepository) {
 
+    @QueryMapping
+    fun employees(): List<Employee> {
+        return employeeRepository.findAll()
+    }
+
+    @MutationMapping
+    fun addEmployee(@Argument input: EmployeeInput): Employee {
+        val employee = Employee(
+            name = input.name,
+            position = input.position,
+            department = input.department
+        )
+        return employeeRepository.save(employee)
+    }
+
+    
 }
